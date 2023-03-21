c78b6251a37c7fb2ac39d7abaeafa72182198dd9
Vadim Mironov <mrnvadim19@gmail.com>
2023-03-22 01:02:51 +0300

    1.Исправил конструкцию проверки токенов. 2.Поправил логгеры. 3.Добавил дебагеры в функцию get_api_answer. 4.Добавил проверку ключа current_date 5.Поправил основной цикл while

--- homework.py
+++ homework.py
@@ -11,6 +11,16 @@ from dotenv import load_dotenv
 from exceptions import (InvalidCodeStatus, InvalidResponseAPI,
                         TelegramMessageError)
 
+logging.basicConfig(
+    filename='main.log',
+    filemode='w',
+    format='%(asctime)s [%(levelname)s] [%(funcName)s] %(message)s'
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(handler)
+
 load_dotenv()
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -29,16 +39,28 @@ HOMEWORK_VERDICTS = {
 
 def check_tokens():
     """Проверяет доступность токенов."""
-    return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
+    tokens = {
+        'PRACTICUM_TOKEN': PRACTICUM_TOKEN,
+        'TELEGRAM_TOKEN': TELEGRAM_TOKEN,
+        'TELEGRAM_CHAT_ID': TELEGRAM_CHAT_ID
+    }
+    missing_tokens = []
+    for token in tokens:
+        if not tokens.get(token):
+            missing_tokens.append(token)
+    if missing_tokens:
+        logger.critical(f'Отсутствующие токены: {missing_tokens}')
+        return False
+    return True
 
 
 def send_message(bot, message):
     """Отправка сообщения в Telegram чат."""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
-        logging.debug('Сообщение отправлено!')
+        logger.debug('Сообщение отправлено!')
     except Exception as error:
-        logging.error(f'Ошибка в отправке сообщения: {error}')
+        logger.error(f'Ошибка в отправке сообщения: {error}')
         raise TelegramMessageError(f'Ошибка в отправке сообщения: {error}')
 
 
@@ -48,11 +70,18 @@ def get_api_answer(timestamp):
     try:
         response = requests.get(ENDPOINT, headers=HEADERS, params=payload)
     except Exception as error:
-        logging.error(f'Ошибка запроса основного API: {error}')
+        logger.error(f'Ошибка запроса основного API: {error}')
         raise InvalidResponseAPI(f'Ошибка запроса основного API: {error}')
     if response.status_code != HTTPStatus.OK:
+        logger.critical(
+            f'Ошибка ответа API. Код ответа: {response.status_code}. '
+            f'Параметры: {ENDPOINT}, {HEADERS}, {payload}'
+        )
         raise InvalidCodeStatus(f'Статус кода {response.status_code}')
-    return response.json()
+    try:
+        return response.json()
+    except ValueError as error:
+        raise ValueError(f'Возникала ошибка при преобразовании JSON: {error}')
 
 
 def check_response(response):
@@ -61,6 +90,8 @@ def check_response(response):
         raise TypeError('Ответ от API не является словарем')
     if not isinstance(response.get('homeworks'), list):
         raise TypeError('homeworks не возвращаются в виде списка')
+    if 'current_date' not in response:
+        raise KeyError('В ответе нет current_date')
     return response.get('homeworks')
 
 
@@ -79,39 +110,49 @@ def parse_status(homework):
 def main():
     """Основная логика работы бота."""
     if not check_tokens():
-        logging.critical('Отсутствуют переменные окружения.')
         sys.exit('Критическая ошибка! Отсутствуют токены')
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
+    sent_message = ''
+    sent_error = ''
 
     while True:
         try:
             response = get_api_answer(timestamp)
             homeworks = check_response(response)
+            timestamp = response.get('current_date', timestamp)
             if homeworks:
-                send_message(bot, parse_status(homeworks[0]))
+                message = parse_status(homeworks[0])
+                if message != sent_message:
+                    send_message(bot, message)
+                sent_message = message
             else:
                 logger.debug('Статус домашней работы не изменился')
-            timestamp = response.get('current_date', timestamp)
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            send_message(bot, message)
-            logger.error(message)
+            message_error = f'Сбой в работе программы: {error}'
+            send_message(bot, message_error)
+            logger.error(message_error)
+            if message_error != sent_error:
+                send_message(bot, message_error)
+                sent_message = message_error
         finally:
             time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
-    logging.basicConfig(
-        filename='main.log',
-        filemode='w',
-        format='%(asctime)s [%(levelname)s] %(message)s'
+    # Перенес наверх, иначе падает pytest
+    # Вопрос: а где лучше размещать блок кода с logger?
+    # В main или просто в начале файла?
 
-    )
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    handler = logging.StreamHandler(sys.stdout)
-    logger.addHandler(handler)
+    # logging.basicConfig(
+    #     filename='main.log',
+    #     filemode='w',
+    #     format='%(asctime)s [%(levelname)s] [%(funcName)s] %(message)s'
+    # )
+    # logger = logging.getLogger(__name__)
+    # logger.setLevel(logging.DEBUG)
+    # handler = logging.StreamHandler(sys.stdout)
+    # logger.addHandler(handler)
 
     main()
